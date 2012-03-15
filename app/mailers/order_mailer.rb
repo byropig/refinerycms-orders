7689fe3a0e0f9b65d13ea06b95f4c39ed1d34507
byron <byron@perfectcircle.co.za>
2012-03-15 12:43:53 +0200

    more refinery setting changes

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -3,7 +3,7 @@ class OrderMailer < ActionMailer::Base
   def admin_new_order(customer, order)
     subject     "New Cash Order Received"
     recipients  RefinerySetting.find_or_set(:order_notification_recipients, "byron@perfectcircle.co.za, james@perfectcircle.co.za")
-    from        "<no-reply@redontop.co.za>"    
+    from        RefinerySetting.find_or_set(:order_notification_from, "<no-reply@example.com>")    
     @customer =  customer
     @order = order
     
@@ -15,7 +15,7 @@ class OrderMailer < ActionMailer::Base
   def admin_credit_received(customer, order)
     subject     "Credit Card Payment for Order Approved"
     recipients  RefinerySetting.find_or_set(:order_notification_recipients, "byron@perfectcircle.co.za, james@perfectcircle.co.za")
-    from        "<no-reply@redontop.co.za>"    
+    from        RefinerySetting.find_or_set(:order_notification_from, "<no-reply@example.com>")   
     @customer =  customer
     @order = order
     
@@ -27,7 +27,7 @@ class OrderMailer < ActionMailer::Base
   def customer_new_order(customer, order)
     subject     "New Cash Order"
     recipients  customer.email
-    from        "<no-reply@redontop.co.za>"    
+    from        RefinerySetting.find_or_set(:order_notification_from, "<no-reply@example.com>")
     @customer =  customer
     @order = order
     
@@ -39,7 +39,7 @@ class OrderMailer < ActionMailer::Base
   def customer_new_credit_order(customer, order)
     subject     "New Credit Card Order"
     recipients  customer.email
-    from        "<no-reply@redontop.co.za>"    
+    from        RefinerySetting.find_or_set(:order_notification_from, "<no-reply@example.com>")
     @customer =  customer
     @order = order
     
@@ -51,7 +51,7 @@ class OrderMailer < ActionMailer::Base
   def customer_cash_received(customer, order)
     subject     "Payment for Order Received"
     recipients  customer.email
-    from        "<no-reply@redontop.co.za>"    
+    from        RefinerySetting.find_or_set(:order_notification_from, "<no-reply@example.com>")
     @customer =  customer
     @order = order
     
